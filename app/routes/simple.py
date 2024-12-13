701311e3866d08962afa30866dca00fb6cdbb1cc
daniel watson <ozzloy@each.do>
2024-12-12 18:20:20 -0800

    create simple form, route to it

--- app/routes/simple.py
+++ app/routes/simple.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from ..forms import SimpleForm
 
 bp = Blueprint("main", __name__, "/")
 
@@ -6,3 +7,9 @@ bp = Blueprint("main", __name__, "/")
 @bp.route("/")
 def index():
     return render_template("main_page.html.j2")
+
+
+@bp.route("/simple-form")
+def simple_form():
+    form = SimpleForm()
+    return render_template("simple_form.html.j2", form=form)
