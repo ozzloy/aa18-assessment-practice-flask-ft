3447ff21e78cfb28afa22892a7eb47743bdea5a8
daniel watson <ozzloy@each.do>
2024-12-13 14:53:05 -0800

    create bare minimum form with csrf

--- app/routes/simple.py
+++ app/routes/simple.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
 
+from app.forms import SimpleForm
+
 bp = Blueprint("simple", __name__)
 
 
 @bp.route("/")
 def index():
     return render_template("main_page.html.j2")
+
+
+@bp.route("/simple-form")
+def simple_form():
+    form = SimpleForm()
+    return render_template("simple_form.html.j2", form=form)
