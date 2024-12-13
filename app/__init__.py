14d6815419d81810d11e5b8f7345e0a38626eae0
daniel watson <ozzloy@each.do>
2024-12-13 14:50:32 -0800

    add configuration and SECRET_KEY with it

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+
+from app.config import Configuration
 from .routes.simple import bp
 
 app = Flask(__name__)
 app.register_blueprint(bp)
+app.config.from_object(Configuration)
