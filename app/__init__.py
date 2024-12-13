35bb254c0661bb1c157c5ba946af21a7ec3a2411
daniel watson <ozzloy@each.do>
2024-12-12 18:34:51 -0800

    init db, inherit from db.Model, SimplePerson.id nullable=False

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 from app.routes import simple
 from app.config import Configuration
 
+from app.models import db
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(simple.bp)
+db.init_app(app)
