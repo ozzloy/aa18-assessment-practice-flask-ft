31df7eda9c6bafb7a9a9b5747a92434f26abcdd5
daniel watson <ozzloy@each.do>
2024-12-13 19:05:28 -0800

    include Migrate for flask db init,migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 from app.config import Configuration
 from app.routes.simple import bp
@@ -10,3 +11,5 @@ app.config.from_object(Configuration)
 from app.models import db
 
 db.init_app(app)
+
+Migrate(app, db)
