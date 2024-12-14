01576bc603a0af93ec347067012c6ed8c4ae14c3
daniel watson <ozzloy@each.do>
2024-12-14 15:20:25 -0800

    make migration to add table for simple_people

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+
 from app.routes.simple import bp
 from app.config import Configuration
+
 
 app = Flask(__name__)
 app.register_blueprint(bp)
 app.config.from_object(Configuration)
+
+from app.models import db
+
+db.init_app(app)
+
+Migrate(app, db)
--- app/config.py
+++ app/config.py
@@ -3,3 +3,6 @@ from os import environ
 
 class Configuration:
     SECRET_KEY = environ.get("SECRET_KEY", "set SECRET_KEY in .env")
+    SQLALCHEMY_DATABASE_URI = environ.get(
+        "DATABASE_URL", "set DATABASE_URL in .env"
+    )
