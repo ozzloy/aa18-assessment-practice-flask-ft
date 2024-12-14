d81ff7aa3bb04684d6b86e907fa4d72e1908ef00
daniel watson <ozzloy@each.do>
2024-12-13 19:38:45 -0800

    show M named people

--- app/routes/simple.py
+++ app/routes/simple.py
@@ -30,3 +30,13 @@ def post_simple_form():
     db.session.add(person)
     db.session.commit()
     return redirect("/")
+
+
+@bp.route("/simple-form-data")
+def simple_form_data():
+    m_people = SimplePerson.query.filter(
+        SimplePerson.name.like("M%")
+    ).all()
+    return render_template(
+        "simple_form_data.html.j2", people=m_people
+    )
